b99f2b58af091c007927924da4e1e8b0dd9a53f9
Luke Davis <Luke180994@gmail.com>
2021-06-30 20:55:13 +0100

    add featured view modified code so it displays featured recipes

--- Food app/Views/FeaturedView.swift
+++ Food app/Views/FeaturedView.swift
@@ -12,44 +12,66 @@ struct FeaturedView: View {
     @EnvironmentObject var model: RecipeModel
     
     var body: some View {
-       
-        GeometryReader(){ geo in
-            TabView(){
-                
-                ForEach (0..<model.recipes.count) { index in
+        
+        VStack(alignment: .leading, spacing: 0){
+            
+            Text("Featured Recipes")
+                .bold()
+                .padding(.leading)
+                .padding(.top, 40)
+                .font(.largeTitle)
+            
+            GeometryReader(){ geo in
+                TabView(){
                     
-                    if model.recipes[index].featured == true {
+                    ForEach (0..<model.recipes.count) { index in
                         
-                        ZStack(){
+                        if model.recipes[index].featured == true {
                             
-                            Rectangle()
-                                .frame(width: geo.size.width - 40, height: geo.size.height - 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                                cornerRadius(17)
-                                .shadow(radius: 10)
+                            ZStack(){
+                                
+                                Rectangle()
+                                    .foregroundColor(.white)
+                                
+                                
+                                VStack(spacing: 0){
+                                    
+                                    Image(model.recipes[index].image)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .clipped()
+                                    
+                                    Text(model.recipes[index].name)
+                                        .padding(5)
+                                    
+                                    
+                                }
+                                
+                            }.frame(width: geo.size.width - 40, height: geo.size.height - 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                .cornerRadius(17)
+                            .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.5), radius: 10, x: -5, y: 5)
                             
-                            VStack(spacing: 0){
-                                
-                                Image("lasagna")
-                                    .resizable()
-                                    .clipped()
-                                
-                                Text("lasagna")
-                                
-                            }
                             
                         }
-                        
-                        
-                            
                     }
+                    
+                    
                 }
-                
-                
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
             }
-            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
-            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            
+            VStack(alignment: .leading, spacing: 10) {
+                Text("Prep Time:")
+                    .font(.headline)
+                
+                Text("1 Hour")
+                Text("Highlights")
+                    .font(.headline)
+                Text("Healthy, Hearty")
+            }.padding([.leading, .bottom])
+            
         }
-        
         
     }
 }
