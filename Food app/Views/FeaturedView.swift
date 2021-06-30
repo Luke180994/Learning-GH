89298fc6d4102f2752f4775071bccf1c46a0e8cb
Luke Davis <Luke180994@gmail.com>
2021-06-30 19:45:44 +0100

    test

--- Food app/Views/FeaturedView.swift
+++ Food app/Views/FeaturedView.swift
@@ -12,12 +12,51 @@ struct FeaturedView: View {
     @EnvironmentObject var model: RecipeModel
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       
+        GeometryReader(){ geo in
+            TabView(){
+                
+                ForEach (0..<model.recipes.count) { index in
+                    
+                    if model.recipes[index].featured == true {
+                        
+                        ZStack(){
+                            
+                            Rectangle()
+                                .frame(width: geo.size.width - 40, height: geo.size.height - 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                cornerRadius(17)
+                                .shadow(radius: 10)
+                            
+                            VStack(spacing: 0){
+                                
+                                Image("lasagna")
+                                    .resizable()
+                                    .clipped()
+                                
+                                Text("lasagna")
+                                
+                            }
+                            
+                        }
+                        
+                        
+                            
+                    }
+                }
+                
+                
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+        }
+        
+        
     }
 }
 
 struct FeaturedView_Previews: PreviewProvider {
     static var previews: some View {
         FeaturedView()
+            .environmentObject(RecipeModel())
     }
 }
